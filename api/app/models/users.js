78cdb4e1dd326618a815c548618003e3a95b8b12
Stephen KINGER <stephen.kinger@gmail.com>
2016-11-09 20:27:13 +0100

    first implementation for checking users declared

--- api/app/models/users.js
+++ api/app/models/users.js
@@ -1,2 +1,35 @@
 
-// "/etc/openvpn/easy-rsa/keys/index.txt"
+function User() {
+      this.id = 'Unknown';
+      this.name = 'Unknown';
+      this.email = 'Unknown';
+      this.location = 'Unknown';
+      this.state = 'Unknown';
+}
+
+function processFile(inputFile) {
+    var fs = require('fs');
+    var myUserList = [];
+
+    fs.readFileSync(inputFile).toString().match(/^.+$/gm).forEach(function (line) {
+      var splitter = line.split('=');
+      var userRead = new User();
+      userRead.name = splitter[6].split('\/')[0];
+      userRead.email = splitter[8];
+      userRead.location = splitter[3].split('\/')[0];
+      if (/^V/.test(line) == true) {
+        console.log('Valid');
+        userRead.state = 'Valid';
+      }
+      else if(/^R/.test(line) == true) {
+        userRead.state = 'Revoked';
+      }
+      userRead.id = splitter[0].split(' ')[7];
+      myUserList.push(userRead);
+
+    });
+    return myUserList;
+}
+
+exports.User = User;
+exports.processUserFile = processFile;
--- api/server.js
+++ api/server.js
@@ -56,72 +56,47 @@ router.route('/users')
 								{ id: 'new', name : req.body.name, state: 'Valid',
 								location: req.body.location, email: req.body.email}];
 		res.json(user);
-
+//		userFactory(10, "moi", 'titi@fmail', 'paris', 'valid');
 	})
 
 	// get all the bears (accessed at GET http://localhost:8080/api/users)
 	.get(function(req, res) {
-		console.log('get users');
-// 		V       260924094424Z           01      unknown /C=FR/ST=FR/L=Paris/O=None/OU=PiVPN/CN=pivpn/name=EasyRSA/emailAddress=myemail@
-		var user = [{ id: '260924094424Z', name: 'pivpn', state: 'Valid',
-									location: 'Paris', email: 'myemail@mail'},
-								{ id: 'none', name: 'other', state: 'Revoked',
-								location: 'Moon', email: 'moon@mail'},
-								{ id: 'titi', name: 'other', state: 'Valid',
- 								location: 'Moon', email: 'sun@mail'}];
-		res.json(user);
+		userList = Users.processUserFile("./api/app/models/test.txt");
+		res.json(userList);
 	});
 
-// on routes that end in /users/:user_id
-// ----------------------------------------------------
+/**
+ * On routes that end in /users/:user_id
+ * @param {Object} employee - The employee who is responsible for the project.
+ * @param {string} employee.name - The name of the employee.
+ * @param {string} employee.department - The employee's department.
+ */
 router.route('/users/:user_id')
 
-	// get the user with that id
-	.get(function(req, res) {
+	/**
+	 * User getter
+	 * @memberof users
+	 * @function
+	 * @name.
+	 */
+ 	.get(function(req, res) {
 		res.json({name: 'Anonymous'});
-		// Bear.findById(req.params.bear_id, function(err, bear) {
-		// 	if (err)
-		// 		res.send(err);
-		// 	res.json(bear);
-		// });
 	})
 
 	// update the user with this id
 	.put(function(req, res) {
-		// Bear.findById(req.params.bear_id, function(err, bear) {
-		//
-		// 	if (err)
-		// 		res.send(err);
-		//
-		// 	bear.name = req.body.name;
-		// 	bear.save(function(err) {
-		// 		if (err)
-		// 			res.send(err);
-		//
-		// 		res.json({ message: 'Bear updated!' });
-		// 	});
-		//
 		res.json({ message: 'Not Implemented Yet!' });
-		// });
 	})
 
 	// delete the bear with this id
 	.delete(function(req, res) {
-		// Bear.remove({
-		// 	_id: req.params.bear_id
-		// }, function(err, bear) {
-		// 	if (err)
-		// 		res.send(err);
-		//
-		// 	res.json({ message: 'Successfully deleted' });
 		res.json({ message: 'Not Implemented Yet!' });
-		// });
-
 	 });
 
 
 // REGISTER OUR ROUTES -------------------------------
 app.use('/api', router);
+
 
 // START THE SERVER
 // =============================================================================
