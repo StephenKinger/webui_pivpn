cd0b57608103b3af832091db21839603669121e4
Stephen KINGER <stephen.kinger@gmail.com>
2016-11-12 16:00:13 +0000

    Modified server.js

--- api/server.js
+++ api/server.js
@@ -82,6 +82,40 @@ router.post('/authenticate', function(req, res) {
 	}
 });
 
+// ---------------------------------------------------------
+// route middleware to authenticate and check token
+// ---------------------------------------------------------
+router.use(function(req, res, next) {
+
+	// check header or url parameters or post parameters for token
+	var token = req.body.token || req.param('token') || req.headers['x-access-token'];
+
+	// decode token
+	if (token) {
+
+		// verifies secret and checks exp
+		jwt.verify(token, app.get('superSecret'), function(err, decoded) {			
+			if (err) {
+				return res.json({ success: false, message: 'Failed to authenticate token.' });		
+			} else {
+				// if everything is good, save to request for use in other routes
+				req.decoded = decoded;	
+				next();
+			}
+		});
+
+	} else {
+
+		// if there is no token
+		// return an error
+		return res.status(403).send({ 
+			success: false, 
+			message: 'No token provided.'
+		});
+		
+	}
+	
+});
 
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
