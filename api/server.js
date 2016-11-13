8ddfb9ab1265d556472d181c787a965f1697e5e5
Stephen KINGER <stephen.kinger@gmail.com>
2016-11-13 16:33:51 +0000

    Modified server.js

--- api/server.js
+++ api/server.js
@@ -25,7 +25,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, "../app/dist")));
+app.use(express.static(path.join(__dirname, "../dist")));
 
 var port     = process.env.PORT || 3000; // set our port
 
